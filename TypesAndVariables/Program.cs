857e98b2356be58f37d40dcc98dcb293b9b55ef3
Eren Macit <ernmctt@gmail.com>
2023-10-23 23:04:09 +0300

    Add project files.

---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Value Types
+            //Console.WriteLine("Hello World!");
+            int number1 = 1000000000; //-2 147 583 647 ve 2 147 583 647 
+            Console.WriteLine("Number1 is {0}", number1);
+            Console.ReadLine();
+
+
+        }
+    }
+}
