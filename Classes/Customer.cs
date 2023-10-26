48f15fbff2e52e7c4a4b62a2281deec2fe6f9918
Eren Macit <ernmctt@gmail.com>
2023-10-26 17:45:10 +0300

    Property-field, encapsulation

---
+++ Classes/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Customer
+    {
+        //field tanımlamak
+        public string FirstNameField;
+
+        //prop(property) tanımlama
+        //kısayol - prop + tab
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+        //Neden prop olarak tanımlanıyor? (get, set)
+        //encapsulation
+        public int Id2 { get; set; }
+
+        private string _firstName;
+        public string FirstName2
+        {
+            get { return "Mr. " + _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName2 { get; set; }
+        public string City2 { get; set; }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -19,6 +19,24 @@ namespace Classes
             ProductManager productManager = new ProductManager();
             productManager.Add();
             productManager.Update();
+
+            //class oluşturma
+            Customer customer = new Customer();
+            //class proplarına değer atama - set bloğu
+            customer.City = "Ankara";
+            customer.Id = 1;
+            customer.FirstName = "Eren";
+            customer.LastName = "Macit";
+
+            //class oluşturmanın farklı(daha kısa) yolu 
+            Customer customer2 = new Customer
+            {
+               Id2 = 2, City2 = "Istanbul", FirstName2 = "Macit", LastName2 = "Eren"
+            };
+            //customer2 classının firstname propunu yazdırma - get bloğu
+            Console.WriteLine(customer2.FirstName2);
+
+            Console.ReadLine();
         }
     }
 }
